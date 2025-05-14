9af73c43c667172269287906e6a45e18cd7a5d82
Gabriel Tobing <6481213l@gmail.com>
2025-05-14 18:05:08 +1000

    import json to ts file issue

--- gabtobing-web/assets/scripts/stream-schedule.ts
+++ gabtobing-web/assets/scripts/stream-schedule.ts
@@ -1,23 +1,26 @@
-import schedule from '../datasets/upcoming-stream-info.json'
+import stream_schedule_data from '../datasets/upcoming-stream-info.json'
 
-function create_card(img: string, title: string, description: string, time: string, video_link: string): string {
-    return `
-        <!-- Individual card layout slots -->
-        <div class="col">
-        <div class="card" style="width: 20rem">
-            <h5 class="card-title text-center pt-3 pb-2"> ${Date} </h5>
-            <img class="container-fluid" src="${img}">
-            <div class="p-3">
-               <h3 class="class-title">${title}</h3>
-               <p class="card-text">${description}</p>
-               <div class="container-fluid text-center">
-                   <a href="${video_link}" class="container-fluid btn btn-primary">View stream!</a>
-               </div>
-            </div>
-        </div>
-        </div>
-    `
-}
+// function create_card(img: string, title: string, description: string, time: string, video_link: string): string {
+//     return `
+//         <!-- Individual card layout slots -->
+//         <div class="col">
+//         <div class="card" style="width: 20rem">
+//             <h5 class="card-title text-center pt-3 pb-2"> ${Date} </h5>
+//             <img class="container-fluid" src="${img}">
+//             <div class="p-3">
+//                <h3 class="class-title">${title}</h3>
+//                <p class="card-text">${description}</p>
+//                <div class="container-fluid text-center">
+//                    <a href="${video_link}" class="container-fluid btn btn-primary">View stream!</a>
+//                </div>
+//             </div>
+//         </div>
+//         </div>
+//     `
+// }
+
+// Verifying it's working 
+console.log('Stream-schedule script working! - TS mode')
 
 // Defining interface for the JSON structure
 
@@ -30,15 +33,16 @@ let time = "test4"
 let video_link = "test5"
 
 // Processing JSON
-console.log(schedule)
+//console.log(schedule)
 //const schedule_obj = console.log(schedule)
 
 
 // TODO: Fix this typescript specific issue
 
-const card_set = create_card(img, title, description, time, video_link)
-var value
-if (card_set){
-    value = card_set.valueOf
-}
-document.getElementById('stream-schedule-cards-section')!.innerHTML() = var
+//const card_set = create_card(img, title, description, time, video_link)
+
+// Stackoverflow example:
+// https://stackoverflow.com/questions/45712292/how-to-add-innerhtml-in-typescript
+
+//let card_html_container  = document.getElementById('stream-schedule-cards-section') as HTMLInputElement
+//card_html_container.value = card_set
--- gabtobing-web/src/app/home-page/home-page.component.ts
+++ gabtobing-web/src/app/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import * as stream_schedule_data from '../../../assets/datasets/upcoming-stream-info.json'
 
 @Component({
     selector: 'router-outlet',
@@ -8,5 +9,12 @@ import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
     styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+    constructor(){
+        console.log(stream_schedule_data)
+    }
 
+    export_stream_schedule_data(){
+        // Exports stream schedule data for use
+        return stream_schedule_data
+    }
 }
